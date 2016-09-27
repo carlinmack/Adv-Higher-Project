31f08f7ebb8f8ba60fb508aa6f8de37de3e39f25
mackenzieck <mackenzieck@rgc.aberdeen.sch.uk>
2016-09-27 10:19:57 +0100

    fixed syntax error

--- classes.js
+++ classes.js
@@ -35,7 +35,7 @@ class Deck {
 
     cut() {
         //remove last 20% of cards
-        for (let i = 0; i < this.availableCards/5; i++) this.availableCards.pop();
+        for (let i = 0; i < this.availableCards.length/5; i++) this.availableCards.pop();
     }
 
     deal() {
